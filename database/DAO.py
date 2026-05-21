50d7a4b98ca9ea26aa60b70152bcb3d03bd657c7
alessiasassone <alessiasassone04@gmail.com>
2026-05-21 15:22:46 +0200

    prima versione

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,48 @@
 from database.DB_connect import DBConnect
+from model.team import Team
+
+
+#Non serve il costruttore
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getAllYears():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """SELECT DISTINCT t.`year`  
+                    from teams t 
+                    where t.`year` >= 1980 """
+
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(row["year"]) #Non serve oggetto
+
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getTeamsOfYear(year):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """SELECT *
+                    FROM teams t
+                    WHERE t.`year` = %s """
+
+        cursor.execute(query, (year,))
+
+        for row in cursor:
+            result.append(Team(**row))
+
+        cursor.close()
+        conn.close()
+        return result
--- model/model.py
+++ model/model.py
@@ -1,3 +1,36 @@
+import itertools
+
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._grafo = nx.Graph()
+        self._teams = []
+
+    def getAllYears(self):
+        return DAO.getAllYears()
+
+    def getTeamsOfYear(self, year):
+        self._teams = DAO.getTeamsOfYear(year)
+        return self._teams
+
+    def creaGrafo(self):
+        self._grafo.clear()
+        self._grafo.add_nodes_from(self._teams)
+
+        # Per gli archi non scrivo una query perchè tanto mi serve un arco per ogni coppia,
+        # il doppio for non è ideale
+        # for u in self._grafo.nodes:
+        #     for v in self._grafo.nodes:
+        #         if u != v:
+        #             self._grafo.add_edge(u, v)
+
+        myedges = itertools.combinations(self._teams, 2) # mi prendo i team a due a due
+        # Add edges from è un metodo che accetta solo tuple
+        self._grafo.add_edges_from(myedges)
+
+    def getGraphDetails(self):
+        return len(self._grafo.nodes), len(self._grafo.edges)
